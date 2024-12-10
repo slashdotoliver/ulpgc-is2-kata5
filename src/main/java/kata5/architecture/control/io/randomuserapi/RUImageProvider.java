0e8f63df3e6fa4756b4d440f45e265b7938cb367
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2024-12-10 12:08:15 +0000

    Add RUImageProvider

---
+++ src/main/java/kata5/architecture/control/io/randomuserapi/RUImageProvider.java
@@ -0,0 +1,34 @@
+package kata5.architecture.control.io.randomuserapi;
+
+import kata5.architecture.control.io.Provider;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Set;
+
+public class RUImageProvider implements Provider<byte[]> {
+
+    private static final Set<String> RANDOMUSER_URL_PREFIXES = Set.of(
+            "https://randomuser.me/api",
+            "http://randomuser.me/api"
+    );
+
+    private final URL url;
+
+    public RUImageProvider(URL url) {
+        this.url = url;
+    }
+
+    @Override
+    public byte[] retrieve() throws IOException {
+        if (RANDOMUSER_URL_PREFIXES.stream().noneMatch(prefix -> url.toString().startsWith(prefix)))
+            throw new IOException(
+                    "Tried retrieving image from: " +
+                            url.toString() +
+                            "but failed because it did not start with any valid prefix: " +
+                            RANDOMUSER_URL_PREFIXES
+            );
+        return Jsoup.connect(url.toString()).ignoreContentType(true).execute().bodyAsBytes();
+    }
+}
